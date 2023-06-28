8ac1a96d084692d2e8d371485d9bfe1229db2d22
techyberme <107142731+techyberme@users.noreply.github.com>
2023-06-28 10:59:22 +0200

    Update sub.js

--- sub.js
+++ sub.js
@@ -1,23 +1,24 @@
+//código para suscripción y subida de datos
 const mqtt = require('mqtt')
 const mysql=require('mysql')
 
-const db = mysql.createConnection({
-    host: "localhost",
+const db = mysql.createConnection({     //parámetros la base de datos mysql
+    host: "localhost",                  //se encuentra en la línea local
     port: 3306,
-    user: 'root',
-    password:'',
-    database: 'mqttnode'
+    user: 'root',                      
+    password:'',                         
+    database: 'mqttnode'                //nombre de la base de datos
 })
-db.connect(() =>{
+db.connect(() =>{                        //conexión a la base de datos
     console.log('Database ok!')
 })
-const sub = mqtt.connect('mqtt://localhost:9000')
+const sub = mqtt.connect('mqtt://localhost:9000')       //suscripción al borker
 sub.on('connect', ()=>{
-    sub.subscribe('topic test')
+    sub.subscribe('topic test')                          //suscripción al tema 'topic test'
 })
-sub.on('message',(topic,message) =>{
-    message= message.toString()
-    message= message.split(' ')
+sub.on('message',(topic,message) =>{                     //recepción del mensaje
+    message= message.toString()                          //conversión del mensaje a strig
+    message= message.split(' ')                          
     const message1 = parseInt(message[1])
     const message2 = parseInt(message[0])
     console.log(message)
